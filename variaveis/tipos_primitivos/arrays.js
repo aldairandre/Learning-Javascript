4414d4aa02fb86990bd5cac173f698bf39072279
Aldair André <aldairandre99@gmail.com>
2021-05-12 21:37:30 +0100

    Estudando arrays

---
+++ variaveis/tipos_primitivos/arrays.js
@@ -0,0 +1,7 @@
+//Array
+
+//São variaveis capazes de armazenar mais de de um tipo de dados ou valores que podem ser acessados a partir de um indice
+
+let alunoNumero= ['Aldair',1,'Janaina',3,'João',10]
+
+console.log()
