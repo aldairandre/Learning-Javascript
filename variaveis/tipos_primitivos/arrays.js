750a4890661838abd8356c49397bdc3bca604ef5
Aldair Dos Santos <aldairandre99@gmail.com>
2021-05-13 14:38:12 +0100

    Fazendo comparação de valor com array

--- variaveis/tipos_primitivos/arrays.js
+++ variaveis/tipos_primitivos/arrays.js
@@ -11,3 +11,11 @@ console.log(alunoNumero[0])
 //Vendo tamanho do Array
 
 console.log(alunoNumero.length)
+
+//Comaparando valor do array
+
+if(alunoNumero[1]===Number){
+    console.log("São iguais")
+}else{
+    console.log("São diferentes")
+}
