c503f6ed7669e80915d348cbbfe74ba65802d137
Aldair Dos Santos <aldairandre99@gmail.com>
2021-05-17 15:15:20 +0100

    Operadores de Atribuicao

---
+++ Operadores/script2.js
@@ -0,0 +1,20 @@
+//OPERADORES DE ATRIBUIÇÃO
+
+// =, +=
+
+let valorTecladoGamer = 100
+
+console.log(valorTecladoGamer)
+
+//valorTecladoGamer = valorTecladoGamer + valorTecladoGamer 
+
+//Pode ser escrita de uma forma simplificada de com o operador de atribuicao += 
+
+valorTecladoGamer += valorTecladoGamer
+
+console.log(valorTecladoGamer)
+ 
+
+valorTecladoGamer -= valorTecladoGamer
+
+console.log(valorTecladoGamer)
