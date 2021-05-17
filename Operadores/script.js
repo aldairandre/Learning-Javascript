904dcf08733678e9ed7bfc02d7ce6f2cd80b8e6b
Aldair Dos Santos <aldairandre99@gmail.com>
2021-05-17 14:18:20 +0100

    Operadores Aritimétricos

---
+++ Operadores/script.js
@@ -0,0 +1,32 @@
+//OPERADORES EM JAVASCRIPT
+
+//EM JS TEM OS OPERADORES:
+
+    // ARITMÉTRICOS
+    // +, -, *, /,
+
+let salario = 100;
+
+console.log(salario + salario)
+console.log(salario - salario)
+console.log(salario * salario)
+console.log(salario / salario)
+console.log(salario**2)
+
+//OPERADORES DE INCREMWNTO E DECREMENTO
+
+let idade = 18
+
+// Primeiro escreve o valor antigo da variavel e so depois soma o valor da variavel idade por 1 
+
+console.log(idade++) 
+
+console.log(idade) //A idade passa a ser 19
+
+//Caso o queira que seja feito primeiro a soma deve se add o sinal de ++ antes da varaivel
+
+let idade2 = 20
+
+console.log(--idade) //A subtração é feito primeiro
+
+
