104a07c3bd28aa4c85b3a8bf16914716f703180d
Aldair André <aldairandre99@gmail.com>
2021-05-24 21:46:05 +0100

    Exercicio para trocar valores de variaveis

---
+++ Exercicios/Troca Valores/script-troca-valores.js
@@ -0,0 +1,7 @@
+//Script para trocar valores de varaiaveis
+let a = 10;
+let b = 15;
+let c = a;
+a = b;
+b = c 
+console.log(`Valor inical de A: ${c} \nValor inical de B: ${a}\nTroca\nA: ${a}\nB: ${b}`)
