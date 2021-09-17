818d6d0934e5703cdc3942b6ddbd0856bb87735e
Aldair André <aldairandre99@gmail.com>
2021-09-17 09:32:34 +0100

    Aplicando clean code nos scripts

--- array/forEach/forEach2.js
+++ array/forEach/forEach2.js
@@ -1,10 +1,3 @@
-/**
- *  Entretanto, note que a função de callback não precisa
- necessariamente ser anônima. Podemos defini-la antes e atribuí-la a
- uma variável para passá-la como parâmetro ao forEach :
- */
-
-
 function imprimeNome(nome) {
     console.log(nome);
 }
